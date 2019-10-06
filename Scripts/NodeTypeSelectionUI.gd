9ccaa975cff6ec0aba2afb0383462b0b6d176c9e
svntax <endless.answers@gmail.com>
2019-10-05 23:43:16 -0400

    Add action label to UI

--- Scripts/NodeTypeSelectionUI.gd
+++ Scripts/NodeTypeSelectionUI.gd
@@ -4,12 +4,16 @@ onready var energyNodeIcon = $IconEnergy
 onready var gunNodeIcon = $IconGun
 onready var linkIcon = $IconConnect
 onready var iconHighlight = $IconHighlight
+onready var actionLabel = $ActionRoot/ActionLabel
 
 func selectEnergyNode():
     iconHighlight.position = energyNodeIcon.position
+    actionLabel.set_text("Energy Node")
 
 func selectGunNode():
     iconHighlight.position = gunNodeIcon.position
+    actionLabel.set_text("Gun Node")
 
 func selectLinkIcon():
     iconHighlight.position = linkIcon.position
+    actionLabel.set_text("Connect Nodes")
